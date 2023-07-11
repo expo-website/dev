57f6b691bbad47887f26eb82a7322e6240135886
Punit Gupta <punit.gupta14@yahoo.com>
2023-07-11 13:33:22 +0530

    Add website configuration

---
+++ msft-privacy/packages/507bf267-2d3c-40a9-a82a-e7378c101c5d/latest/website-config.js
@@ -0,0 +1,21 @@
+(function () {
+  window.__MSFT_SDK = window.__MSFT_SDK || {};
+  window.__MSFT_SDK.websiteConfiguration = {
+    name: 'ContosoWebsite',
+    id: '507bf267-2d3c-40a9-a82a-e7378c101c5d',
+    trackerCategories: [
+      {
+        categoryName: 'TestCategory1',
+        id: '0ba4047b-8b91-423f-9bf5-36207dab9f14',
+        isEssential: true,
+        version: 1,
+      },
+      {
+        categoryName: 'TestCategory2',
+        id: '3c4ee90f-1aeb-41d9-9e0e-c18e42040cab',
+        isEssential: false,
+        version: 1,
+      },
+    ],
+  };
+})();
