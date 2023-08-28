dc54b8c2232cae33b9b12bc66ccd7bb1c040246f
Devanshi Karthik <dkarthik@microsoft.com>
2023-08-28 17:06:58 +0530

    added packages

---
+++ msft-privacy/packages/8f548ea2-5fad-474f-aa15-cef9e4cd7b48/latest/core-config.js
@@ -0,0 +1,374 @@
+(function () {
+  window.__MSFT_SDK = window.__MSFT_SDK || {};
+  window.__MSFT_SDK.webConsent = window.__MSFT_SDK.webConsent || {};
+  window.__MSFT_SDK.webConsent.config = {
+    id: '8f548ea2-5fad-474f-aa15-cef9e4cd7b48',
+    kind: 'WebConsent',
+    name: 'PunitPackage-Test2',
+    consentSDKVersion: 1,
+    configurations: {
+      assetsPath: '',
+      headless: false,
+      isGeoTrackingEnabled: true,
+      geoTrackingURL:
+        'https://privacy-afd-test-endpoint-gqagcdh0agaybrfr.z01.azurefd.net',
+      isConsentMasteringEnabled: false,
+      consentMasteringURL: '',
+    },
+    properties: {
+      defaultConsentModel: 'PunitWebCM-Test2',
+      isTrackerBlockingEnabled: true,
+      trackerCategories: [
+        {
+          "version": 2,
+          "objectId": "a3369085-049f-414e-a47d-6eeae5607d39",
+          "properties": { "isEssential": true },
+          "name": "category1",
+          "lastUpdatedDateTime": "2023-06-28T09:09:57.3866897Z"
+        },
+        {
+          "version": 1,
+          "objectId": "9e5bf7d4-ba90-4aa9-a4fc-89ed21c0bccc",
+          "properties": { "isEssential": false },
+          "name": "Performance",
+          "lastUpdatedDateTime": "2023-06-14T07:00:24.2714353Z"
+        },
+        {
+          "version": 2,
+          "objectId": "d4a74258-254a-499b-a98f-49daf8c94d30",
+          "properties": { "isEssential": false },
+          "name": "TCTest12",
+          "lastUpdatedDateTime": "2023-07-05T04:42:56.431433Z"
+        },
+        {
+          "version": 1,
+          "objectId": "16bf31c9-5b5e-469c-8719-006f09092a8a",
+          "properties": { "isEssential": false },
+          "name": "test_Audit",
+          "lastUpdatedDateTime": "2023-06-28T09:29:22.0810051Z"
+        },
+        {
+          "version": 2,
+          "objectId": "0a3f4531-349c-4081-b04f-6cdee9bbe29c",
+          "properties": { "isEssential": false },
+          "name": "test_TC1",
+          "lastUpdatedDateTime": "2023-07-05T04:43:27.3807281Z"
+        },
+        {
+          "version": 2,
+          "objectId": "a8907711-8411-4323-b82d-fef2c5e73bd4",
+          "properties": { "isEssential": false },
+          "name": "TestCategory2",
+          "lastUpdatedDateTime": "2023-07-10T13:06:36.9996708Z"
+        },
+        {
+          "version": 1,
+          "objectId": "3d7fe920-5160-41ad-ab41-545c86fddf45",
+          "properties": { "isEssential": false },
+          "name": "PunitAdvertisement",
+          "lastUpdatedDateTime": "2023-07-11T22:03:12.3119327Z"
+        },
+        {
+          "version": 1,
+          "objectId": "af6f59d8-bb38-498f-9bea-1001106893b1",
+          "properties": { "isEssential": false },
+          "name": "TCNewArch",
+          "lastUpdatedDateTime": "2023-07-17T10:21:08.4615782Z"
+        },
+        {
+          "version": 1,
+          "objectId": "84c4581c-1b14-4d25-a3a4-aa60b98e5147",
+          "properties": { "isEssential": false },
+          "name": "tc1",
+          "lastUpdatedDateTime": "2023-07-31T07:27:12.5175083Z"
+        },
+        {
+          "version": 1,
+          "objectId": "29ace6f7-118f-4615-ba14-a789faa720f2",
+          "properties": { "isEssential": false },
+          "name": "testTC1",
+          "lastUpdatedDateTime": "2023-08-01T09:23:32.5104183Z"
+        },
+        {
+          "version": 2,
+          "objectId": "dce36ed2-ab7a-4429-b573-ca9446f3a1a6",
+          "properties": { "isEssential": true },
+          "name": "TestCategory",
+          "lastUpdatedDateTime": "2023-08-01T09:41:00.8561218Z"
+        },
+        {
+          "version": 1,
+          "objectId": "1c902d49-b00a-49f3-9a7c-624f74fb840d",
+          "properties": { "isEssential": false },
+          "name": "TCPran",
+          "lastUpdatedDateTime": "2023-08-01T09:32:07.6812651Z"
+        },
+        {
+          "version": 3,
+          "objectId": "7026df91-3924-40a5-889c-c569220c3e72",
+          "properties": { "isEssential": true },
+          "name": "PunitAdvertisement-BugBash-2",
+          "lastUpdatedDateTime": "2023-08-01T09:45:26.6801375Z"
+        },
+        {
+          "version": 2,
+          "objectId": "d0f2c278-46e4-41ba-858d-c80601be735f",
+          "properties": { "isEssential": false },
+          "name": "TCTran",
+          "lastUpdatedDateTime": "2023-08-01T12:09:42.271839Z"
+        },
+        {
+          "version": 1,
+          "objectId": "a622268e-a92e-413e-97b2-13b8b580e903",
+          "properties": { "isEssential": false },
+          "name": "PreetTC",
+          "lastUpdatedDateTime": "2023-08-01T10:03:03.0231391Z"
+        },
+        {
+          "version": 2,
+          "objectId": "4f938431-0fd5-4022-ba30-6585e4b6d634",
+          "properties": { "isEssential": false },
+          "name": "TCTran1",
+          "lastUpdatedDateTime": "2023-08-01T10:49:18.5165015Z"
+        },
+        {
+          "version": 2,
+          "objectId": "fdda16ba-ef04-434b-8366-0a2463a5041c",
+          "properties": { "isEssential": false },
+          "name": "TCTran2",
+          "lastUpdatedDateTime": "2023-08-01T13:20:41.811645Z"
+        },
+        {
+          "version": 1,
+          "objectId": "5b0769a5-ad6c-4a67-bb03-b5a4a8ff9a6d",
+          "properties": { "isEssential": false },
+          "name": "PunitAdvertisement-BugBash-32",
+          "lastUpdatedDateTime": "2023-08-07T10:22:49.47513Z"
+        },
+        {
+          "version": 1,
+          "objectId": "db461b42-3fd9-4423-8dc3-c9daa2d3643f",
+          "properties": { "isEssential": true },
+          "name": "lalitAdvertisement-BugBash-4",
+          "lastUpdatedDateTime": "2023-08-07T09:56:16.4478014Z"
+        },
+        {
+          "version": 1,
+          "objectId": "1f63a4db-2311-408f-a65b-eafce60b4476",
+          "properties": { "isEssential": true },
+          "name": "kptc1",
+          "lastUpdatedDateTime": "2023-08-16T03:45:44.1392909Z"
+        },
+        {
+          "version": 1,
+          "objectId": "6d4c0037-2ad0-4cca-b716-6079423eb282",
+          "properties": { "isEssential": true },
+          "name": "kptc2",
+          "lastUpdatedDateTime": "2023-08-16T04:57:45.1427816Z"
+        }
+      ]
+    },
+    models: [
+      {
+        name: 'PunitWebCM-Test2',
+        id: 'a7057a97-f60b-4c5f-97c0-e7c6afcb9efc',
+        kind: 'WebConsent',
+        properties: {
+          consentId: 'ab346df9-e517-4d5e-a372-cab5a2697a94',
+          consentVersion: 1,
+          defaultLCID: 'en',
+          consentType: 'Explicit',
+        },
+        attributes: [
+          { key: 'CountriesOrRegions', value: ['IN'] },
+          { key: 'GeoLocations', value: ['Asia/Kolkata', 'Asia/Calcutta'] },
+        ],
+        headless: false,
+        controls: [
+          {
+            name: 'TrackerCategory',
+            instances: [
+              {
+                version: 2,
+                objectId: 'a3369085-049f-414e-a47d-6eeae5607d39',
+                properties: { isEssential: true },
+                name: 'category1',
+                validations: null,
+                lastUpdatedDateTime: '2023-06-28T09:09:57.3866897Z',
+              },
+              {
+                version: 1,
+                objectId: '9e5bf7d4-ba90-4aa9-a4fc-89ed21c0bccc',
+                properties: { isEssential: false },
+                name: 'Performance',
+                validations: null,
+                lastUpdatedDateTime: '2023-06-14T07:00:24.2714353Z',
+              },
+              {
+                version: 2,
+                objectId: 'd4a74258-254a-499b-a98f-49daf8c94d30',
+                properties: { isEssential: false },
+                name: 'TCTest12',
+                validations: null,
+                lastUpdatedDateTime: '2023-07-05T04:42:56.431433Z',
+              },
+              {
+                version: 1,
+                objectId: '16bf31c9-5b5e-469c-8719-006f09092a8a',
+                properties: { isEssential: false },
+                name: 'test_Audit',
+                validations: null,
+                lastUpdatedDateTime: '2023-06-28T09:29:22.0810051Z',
+              },
+              {
+                version: 2,
+                objectId: '0a3f4531-349c-4081-b04f-6cdee9bbe29c',
+                properties: { isEssential: false },
+                name: 'test_TC1',
+                validations: null,
+                lastUpdatedDateTime: '2023-07-05T04:43:27.3807281Z',
+              },
+              {
+                version: 2,
+                objectId: 'a8907711-8411-4323-b82d-fef2c5e73bd4',
+                properties: { isEssential: false },
+                name: 'TestCategory2',
+                validations: null,
+                lastUpdatedDateTime: '2023-07-10T13:06:36.9996708Z',
+              },
+              {
+                version: 1,
+                objectId: '3d7fe920-5160-41ad-ab41-545c86fddf45',
+                properties: { isEssential: false },
+                name: 'PunitAdvertisement',
+                validations: null,
+                lastUpdatedDateTime: '2023-07-11T22:03:12.3119327Z',
+              },
+              {
+                version: 1,
+                objectId: 'af6f59d8-bb38-498f-9bea-1001106893b1',
+                properties: { isEssential: false },
+                name: 'TCNewArch',
+                validations: null,
+                lastUpdatedDateTime: '2023-07-17T10:21:08.4615782Z',
+              },
+              {
+                version: 1,
+                objectId: '84c4581c-1b14-4d25-a3a4-aa60b98e5147',
+                properties: { isEssential: false },
+                name: 'tc1',
+                validations: null,
+                lastUpdatedDateTime: '2023-07-31T07:27:12.5175083Z',
+              },
+              {
+                version: 1,
+                objectId: '29ace6f7-118f-4615-ba14-a789faa720f2',
+                properties: { isEssential: false },
+                name: 'testTC1',
+                validations: null,
+                lastUpdatedDateTime: '2023-08-01T09:23:32.5104183Z',
+              },
+              {
+                version: 2,
+                objectId: 'dce36ed2-ab7a-4429-b573-ca9446f3a1a6',
+                properties: { isEssential: true },
+                name: 'TestCategory',
+                validations: null,
+                lastUpdatedDateTime: '2023-08-01T09:41:00.8561218Z',
+              },
+              {
+                version: 1,
+                objectId: '1c902d49-b00a-49f3-9a7c-624f74fb840d',
+                properties: { isEssential: false },
+                name: 'TCPran',
+                validations: null,
+                lastUpdatedDateTime: '2023-08-01T09:32:07.6812651Z',
+              },
+              {
+                version: 3,
+                objectId: '7026df91-3924-40a5-889c-c569220c3e72',
+                properties: { isEssential: true },
+                name: 'PunitAdvertisement-BugBash-2',
+                validations: null,
+                lastUpdatedDateTime: '2023-08-01T09:45:26.6801375Z',
+              },
+              {
+                version: 2,
+                objectId: 'd0f2c278-46e4-41ba-858d-c80601be735f',
+                properties: { isEssential: false },
+                name: 'TCTran',
+                validations: null,
+                lastUpdatedDateTime: '2023-08-01T12:09:42.271839Z',
+              },
+              {
+                version: 1,
+                objectId: 'a622268e-a92e-413e-97b2-13b8b580e903',
+                properties: { isEssential: false },
+                name: 'PreetTC',
+                validations: null,
+                lastUpdatedDateTime: '2023-08-01T10:03:03.0231391Z',
+              },
+              {
+                version: 2,
+                objectId: '4f938431-0fd5-4022-ba30-6585e4b6d634',
+                properties: { isEssential: false },
+                name: 'TCTran1',
+                validations: null,
+                lastUpdatedDateTime: '2023-08-01T10:49:18.5165015Z',
+              },
+              {
+                version: 2,
+                objectId: 'fdda16ba-ef04-434b-8366-0a2463a5041c',
+                properties: { isEssential: false },
+                name: 'TCTran2',
+                validations: null,
+                lastUpdatedDateTime: '2023-08-01T13:20:41.811645Z',
+              },
+              {
+                version: 1,
+                objectId: '5b0769a5-ad6c-4a67-bb03-b5a4a8ff9a6d',
+                properties: { isEssential: false },
+                name: 'PunitAdvertisement-BugBash-32',
+                validations: null,
+                lastUpdatedDateTime: '2023-08-07T10:22:49.47513Z',
+              },
+              {
+                version: 1,
+                objectId: 'db461b42-3fd9-4423-8dc3-c9daa2d3643f',
+                properties: { isEssential: true },
+                name: 'lalitAdvertisement-BugBash-4',
+                validations: null,
+                lastUpdatedDateTime: '2023-08-07T09:56:16.4478014Z',
+              },
+              {
+                version: 1,
+                objectId: '1f63a4db-2311-408f-a65b-eafce60b4476',
+                properties: { isEssential: true },
+                name: 'kptc1',
+                validations: null,
+                lastUpdatedDateTime: '2023-08-16T03:45:44.1392909Z',
+              },
+              {
+                version: 1,
+                objectId: '6d4c0037-2ad0-4cca-b716-6079423eb282',
+                properties: { isEssential: true },
+                name: 'kptc2',
+                validations: null,
+                lastUpdatedDateTime: '2023-08-16T04:57:45.1427816Z',
+              },
+            ],
+          },
+        ],
+        translations: ['en'],
+        layout: {
+          name: 'ConsentModelDefaultBannerLayout',
+          id: 'ConsentModelDefaultBannerLayout',
+          version: 1,
+          attributes: {},
+        },
+        version: 1,
+      },
+    ],
+    version: 1,
+  };
+})();
